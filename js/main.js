057197894abc00026f67bcb5e99fd9b28967356e
Alissa Dicaire <dica0020@algonquinlive.com>
2014-09-30 10:20:01 -0400

    got rid of old files

--- js/main.js
+++ js/main.js
@@ -1,3 +1,1 @@
-//alert('Yeah, it works!'); /* Alert is a function because it has open and close brackets right after a word and it is reusable*/
-
-prompt('What is your name?');
+// JavaScript Document
