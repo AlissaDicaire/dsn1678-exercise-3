6e426b20ff2aad6da6dad0214fb42c104d01ea08
Alissa Dicaire <dica0020@algonquinlive.com>
2014-09-30 11:23:40 -0400

    Did everything

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
-// JavaScript Document
+var box = $('.box');
+var showBox = $('.show_box');
+
+var buttonClickHandler = function () { 
+	var isActive = box.attr('data-state');
+	
+	if (isActive == 'active') {
+		box.attr('data-state', 'inactive');
+	} else {
+		box.attr('data-state', 'active');
+	}
+	
+};
+
+showBox.on('click', buttonClickHandler);
+
+/*___________*/
+
+var box2 = $('.box2');
+var showBox2 = $('.show_box2');
+
+var buttonClickHandler = function () { 
+	var isActive = box2.attr('data-state');
+	
+	if (isActive == 'active') {
+		box2.attr('data-state', 'inactive');
+	} else {
+		box2.attr('data-state', 'active');
+	}
+	
+};
+
+showBox2.on('click', buttonClickHandler);
+
+
+
